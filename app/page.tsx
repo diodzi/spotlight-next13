3871a574a83cb5defdc3c25ad354b29e4b7b7aa1
diodzi <diodzi@outlook.com>
2023-03-27 13:20:26 -0400

    we tailwind now (slightly less DDD:)

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin']})
 
 export default function Home() {
   return (
-    <main>
-      we react now DDDDD:
+    <main className={inter.className}>
+      <div className='text-3xl font-bold underline'>
+        we react + tailwind now (slightly less DDD:)
+      </div>
     </main>
   )
 }
