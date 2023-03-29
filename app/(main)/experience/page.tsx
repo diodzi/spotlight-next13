f687cd2c6f7f666b1a9a4283708d3e5bc008c72d
diodzi <diodzi@outlook.com>
2023-03-29 03:18:55 -0400

    hard coded values -> server rendered from sanity

--- app/(main)/experience/page.tsx
+++ app/(main)/experience/page.tsx
@@ -1,45 +1,57 @@
 import ExperienceContainer from './ExperienceContainer'
 import { createClient } from 'next-sanity'
+import { PortableText } from '@portabletext/react'
 
-const client = createClient({
-  projectId: 'vuk3eh3d',
-  dataset: 'production',
-  apiVersion: '2022-03-27',
-})
+type ExperiencePageDataTypes = {
+  experiences: {
+    name: string
+    position: string
+    timeframe: string
+    description: any[]
+  }[]
+  pageDetails: { blurb: any[] }
+}
 
-async function getExperiences() {
-  const experiences = await client.fetch(
-    `*[_type == "experience"] | order(startdate desc)`
+async function getExperiencePageData(): Promise<ExperiencePageDataTypes> {
+  const client = createClient({
+    projectId: 'vuk3eh3d',
+    dataset: 'production',
+    apiVersion: '2022-03-27',
+  })
+
+  const data = await client.fetch(
+    `*[_type == "page" && name == "Experience"]
+    {
+      "pageDetails": *[_type == "pageDetail" && references(^._id)]
+      {blurb}
+      [0],
+      "experiences": *[_type == "experience"] | order(startdate desc)
+      {name, position, timeframe, description}
+    }[0]`
   )
-  if (experiences) {
-    return {
-      experiences,
-    }
-  }
+  return data
 }
 
 export default async function ExperiencePage() {
-  const data = await getExperiences()
+  const { experiences, pageDetails }: ExperiencePageDataTypes =
+    await getExperiencePageData()
 
   return (
     <div className="flex flex-col items-center p-6">
       <h1 className="text-3xl font-bold mb-6">Experience</h1>
-      <p className="text-center">
-        This is a complete list of every professional position I have ever held.
-      </p>
+      <PortableText value={pageDetails.blurb} />
       <div className="flex flex-col gap-5 mt-8">
-        {data?.experiences.map(
-          ({ name, position, timeframe, description }: any) => {
-            return (
-              <ExperienceContainer
-                name={name}
-                position={position}
-                timeframe={timeframe}
-                description={description}
-              />
-            )
-          }
-        )}
+        {experiences?.map(({ name, position, timeframe, description }) => {
+          return (
+            <ExperienceContainer
+              name={name}
+              position={position}
+              timeframe={timeframe}
+              description={description}
+              key={name}
+            />
+          )
+        })}
       </div>
     </div>
   )
