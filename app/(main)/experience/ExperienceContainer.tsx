2e72bb822b2d59004198b5de87cc6f1048285670
diodzi <diodzi@outlook.com>
2023-04-03 03:16:28 -0400

    change to do plaintext + new shadow

--- app/(main)/experience/ExperienceContainer.tsx
+++ app/(main)/experience/ExperienceContainer.tsx
@@ -1,4 +1,4 @@
-import { PortableText } from '@portabletext/react'
+import { toPlainText } from '@portabletext/react'
 
 type Props = {
   name: string
@@ -11,13 +11,13 @@ export default function ExperienceContainer(props: Props) {
   const { name, position, timeframe, description } = props || null
 
   return (
-    <div className="w-full bg-kinda-gray p-6 rounded-3xl">
+    <div className="styled-shadow p-6 rounded-3xl">
       <div className=" pb-2 mb-2">
         <h2 className="text-xl font-bold">{name}</h2>
         <strong>{position}</strong>
         <p>{timeframe}</p>
       </div>
-      <PortableText value={description} />
+      <p className="break-words">{toPlainText(description)}</p>
     </div>
   )
 }
